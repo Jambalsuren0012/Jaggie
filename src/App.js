9f96f2228c5a26384149aafe34ea537a145b054b
Jambalsuren0012 <naruto_0212@yahoo.com>
2023-01-11 17:17:34 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { SocialIcon } from 'react-social-icons';
+import { Typewriter } from 'react-simple-typewriter';
+import {motion} from "framer-motion"
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <motion.div 
+      initial={{ x:-1500}}
+      animate={{x: 0}}
+      transition= {{duration:2}}
+      >
+      <SocialIcon url='https://twitter.com/jaketrent' style={{width:100, height:100}}/>
+      <SocialIcon network='facebook' style={{width:100, height:100}}/>
+      </motion.div>
+      <h1 style={{color:"red"}}>
+        <Typewriter words={["Sain uu", "Pinecone"]}/>
+        </h1>
+        
+        
       </header>
     </div>
   );
