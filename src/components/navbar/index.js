c045e7415d011cb519f5e34ba2db517838be7764
Jambalsuren0012 <naruto_0212@yahoo.com>
2023-01-12 17:22:17 +0800

    up

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -7,7 +7,7 @@ import styles from "./style.module.scss"
 const NavbarMenu = () => {
   return (
     <div className={styles.Container}>
-       <Navbar style={{zIndex: 1000}} collapseOnSelect expand="lg" bg="dark" variant="dark">
+       <Navbar style={{zIndex: 1000 ,padding:12}} collapseOnSelect expand="lg" bg="dark" variant="dark">
      
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -16,10 +16,13 @@ const NavbarMenu = () => {
             
           </Nav>
           <Nav>
-            <Nav.Link href="#deets">about</Nav.Link>
-            <Nav.Link href="#deets">about</Nav.Link>
-            <Nav.Link href="#deets">about</Nav.Link>
-            <Nav.Link href="#deets">about</Nav.Link>
+            <Nav.Link href="#deets">Home</Nav.Link>
+            <Nav.Link href="#deets">About</Nav.Link>
+            <Nav.Link href="#deets">Skills</Nav.Link>
+            <Nav.Link href="#deets">Education</Nav.Link>
+            <Nav.Link href="#deets">Work</Nav.Link>
+            <Nav.Link href="#deets">Experience</Nav.Link>
+            <Nav.Link href="#deets">Contact</Nav.Link>
             
           </Nav>
         </Navbar.Collapse>
